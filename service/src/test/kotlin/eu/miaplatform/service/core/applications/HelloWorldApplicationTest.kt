1b6477762c8247b7a69ee6cfd6f4ccecdd3073c8
Stefano Sonzogni <stefano.sonzogni@mia-platform.eu>
2021-11-08 08:32:27 +0100

    update hw application test

--- service/src/test/kotlin/eu/miaplatform/service/core/applications/HelloWorldApplicationTest.kt
+++ service/src/test/kotlin/eu/miaplatform/service/core/applications/HelloWorldApplicationTest.kt
@@ -4,23 +4,20 @@ import assertk.assertThat
 import assertk.assertions.isEqualTo
 import com.fasterxml.jackson.annotation.JsonInclude
 import com.fasterxml.jackson.databind.ObjectMapper
-import eu.miaplatform.commons.client.CrudClientInterface
 import eu.miaplatform.commons.ktor.HeadersToProxy
-import eu.miaplatform.commons.client.RetrofitClient
 import eu.miaplatform.service.baseModule
 import eu.miaplatform.service.core.applications.helloworld.HelloWorldApplication
 import eu.miaplatform.service.core.applications.helloworld.HelloWorldService
 import eu.miaplatform.service.model.ErrorResponse
 import eu.miaplatform.service.model.request.HelloWorldRequestBody
 import eu.miaplatform.service.model.response.HelloWorldResponse
-import eu.miaplatform.service.setupResponse
+import io.kotest.core.spec.style.DescribeSpec
 import io.ktor.http.*
 import io.ktor.server.testing.*
 import io.mockk.clearAllMocks
-import okhttp3.logging.HttpLoggingInterceptor
+import io.mockk.coEvery
+import io.mockk.mockk
 import org.junit.jupiter.api.*
-import org.mockserver.client.MockServerClient
-import org.mockserver.integration.ClientAndServer
 import org.slf4j.event.Level
 
 /**
@@ -28,222 +25,188 @@ import org.slf4j.event.Level
  * You could test separately endpoints and logic in two different test classes.
  */
 @TestInstance(TestInstance.Lifecycle.PER_CLASS)
-class HelloWorldApplicationTest {
+class HelloWorldApplicationTest : DescribeSpec({
 
-    private val objectMapper = ObjectMapper().apply {
+    val objectMapper = ObjectMapper().apply {
         setSerializationInclusion(JsonInclude.Include.NON_NULL)
     }
-    private val host = "localhost"
-    private val port = 3000
-    private var mockServer: MockServerClient = MockServerClient(host, port)
 
-    private val crudClient = RetrofitClient.build<CrudClientInterface>("http://$host:$port", HttpLoggingInterceptor.Level.NONE)
-    private val service = HelloWorldService(crudClient)
+    val service = mockk<HelloWorldService>()
 
-    @BeforeEach
-    fun setUp() {
+    beforeEach {
         clearAllMocks()
     }
 
-    @Nested
-    inner class Get {
-        @Test
-        fun `Get should return success object message`() {
-            withTestApplication({
-                baseModule(Level.DEBUG)
-                install(HelloWorldApplication(service, HeadersToProxy()))
-            }) {
-                handleRequest(HttpMethod.Get, "/hello") {
-                }.apply {
-                    assertThat(response.status()?.value).isEqualTo(HttpStatusCode.OK.value)
+    describe("/hello") {
+        describe("get") {
+            it("should return success object message") {
+                withTestApplication({
+                    baseModule(Level.DEBUG)
+                    install(HelloWorldApplication(service, HeadersToProxy()))
+                }) {
+                    handleRequest(HttpMethod.Get, "/hello") {
+                    }.apply {
+                        assertThat(response.status()?.value).isEqualTo(HttpStatusCode.OK.value)
 
-                    val expectedBody = objectMapper.writeValueAsString(
-                        HelloWorldResponse(
-                            null,
-                            null,
-                            "Hello world!"
+                        val expectedBody = objectMapper.writeValueAsString(
+                            HelloWorldResponse(
+                                null,
+                                null,
+                                "Hello world!"
+                            )
                         )
-                    )
-                    assertThat(response.content).isEqualTo(expectedBody)
+                        assertThat(response.content).isEqualTo(expectedBody)
+                    }
+                }
+            }
+
+            it("should return success object message with query parameter") {
+                withTestApplication({
+                    baseModule(Level.DEBUG)
+                    install(HelloWorldApplication(service, HeadersToProxy()))
+                }) {
+                    handleRequest(HttpMethod.Get, "/hello?queryParam=param") {
+                    }.apply {
+                        assertThat(response.status()?.value).isEqualTo(HttpStatusCode.OK.value)
+
+                        val expectedBody = objectMapper.writeValueAsString(
+                            HelloWorldResponse(
+                                null,
+                                "param",
+                                "Hello world!"
+                            )
+                        )
+                        assertThat(response.content).isEqualTo(expectedBody)
+                    }
                 }
             }
         }
 
-        @Test
-        fun `Get should return success object message with query parameter`() {
-            withTestApplication({
-                baseModule(Level.DEBUG)
-                install(HelloWorldApplication(service, HeadersToProxy()))
-            }) {
-                handleRequest(HttpMethod.Get, "/hello?queryParam=param") {
-                }.apply {
-                    assertThat(response.status()?.value).isEqualTo(HttpStatusCode.OK.value)
-
-                    val expectedBody = objectMapper.writeValueAsString(
-                        HelloWorldResponse(
-                            null,
-                            "param",
-                            "Hello world!"
+        describe("/{pathParam}") {
+            describe("post") {
+                it("should return success object message with path param") {
+                    withTestApplication({
+                        baseModule(Level.DEBUG)
+                        install(HelloWorldApplication(service, HeadersToProxy()))
+                    }) {
+                        val body = objectMapper.writeValueAsString(
+                            HelloWorldRequestBody("name", "surname")
                         )
-                    )
-                    assertThat(response.content).isEqualTo(expectedBody)
+
+                        handleRequest(HttpMethod.Post, "/hello/1234") {
+                            addHeader("Content-Type", "application/json")
+                            setBody(body)
+                        }.apply {
+                            assertThat(response.status()?.value).isEqualTo(HttpStatusCode.OK.value)
+
+                            val expectedBody = objectMapper.writeValueAsString(
+                                HelloWorldResponse(
+                                    "1234",
+                                    null,
+                                    "Hello world name surname!"
+                                )
+                            )
+                            assertThat(response.content).isEqualTo(expectedBody)
+                        }
+                    }
+                }
+
+                it("should return bad request if body is malformed") {
+                    withTestApplication({
+                        baseModule(Level.DEBUG)
+                        install(HelloWorldApplication(service, HeadersToProxy()))
+                    }) {
+                        val body = objectMapper.writeValueAsString(
+                            mapOf("name" to "name")
+                        )
+
+                        handleRequest(HttpMethod.Post, "/hello/1234") {
+                            addHeader("Content-Type", "application/json")
+                            setBody(body)
+                        }.apply {
+                            assertThat(response.status()?.value).isEqualTo(HttpStatusCode.BadRequest.value)
+                            val servicePackage = "eu.miaplatform.service"
+                            val expectedBody = objectMapper.writeValueAsString(
+                                // if you change the package name, remember to update it in the following error or the test will fail
+                                ErrorResponse(1000, "Instantiation of [simple type, class $servicePackage.model.request.HelloWorldRequestBody] value failed for JSON property surname due to missing (therefore NULL) value for creator parameter surname which is a non-nullable type\n at [Source: (InputStreamReader); line: 1, column: 15] (through reference chain: $servicePackage.model.request.HelloWorldRequestBody[\"surname\"])")
+                            )
+                            assertThat(response.content).isEqualTo(expectedBody)
+                        }
+                    }
+                }
+            }
+        }
+
+        describe("/with-call") {
+            describe("get") {
+                it("should return success object message") {
+                    coEvery { service.getBooksByHeaders(mapOf()) }
+                        .returns(listOf("book1", "book2"))
+
+                    withTestApplication({
+                        baseModule(Level.DEBUG)
+                        install(HelloWorldApplication(service, HeadersToProxy()))
+                    }) {
+                        handleRequest(HttpMethod.Get, "/hello/with-call") {
+                        }.apply {
+                            assertThat(response.status()?.value).isEqualTo(HttpStatusCode.OK.value)
+
+                            val expectedBody = objectMapper.writeValueAsString(
+                                HelloWorldResponse(
+                                    null,
+                                    null,
+                                    "Hello world! Book list: book1, book2"
+                                )
+                            )
+                            assertThat(response.content).isEqualTo(expectedBody)
+                        }
+                    }
+                }
+
+                it("should return error if service call fails") {
+                    coEvery { service.getBooksByHeaders(mapOf()) }
+                        .throws(Exception("some error occurred"))
+
+                    withTestApplication({
+                        baseModule(Level.DEBUG)
+                        install(HelloWorldApplication(service, HeadersToProxy()))
+                    }) {
+                        handleRequest(HttpMethod.Get, "/hello/with-call") {
+                        }.apply {
+                            assertThat(response.status()?.value).isEqualTo(HttpStatusCode.InternalServerError.value)
+
+                            val expectedBody = objectMapper.writeValueAsString(
+                                ErrorResponse(1000, "some error occurred")
+                            )
+                            assertThat(response.content).isEqualTo(expectedBody)
+                        }
+                    }
+                }
+
+                it("should return success object message with query parameter") {
+                    coEvery { service.getBooksByHeaders(mapOf()) }
+                        .returns(listOf("book1", "book2"))
+
+                    withTestApplication({
+                        baseModule(Level.DEBUG)
+                        install(HelloWorldApplication(service, HeadersToProxy()))
+                    }) {
+                        handleRequest(HttpMethod.Get, "/hello/with-call?queryParam=param") {
+                        }.apply {
+                            assertThat(response.status()?.value).isEqualTo(HttpStatusCode.OK.value)
+
+                            val expectedBody = objectMapper.writeValueAsString(
+                                HelloWorldResponse(
+                                    null,
+                                    "param",
+                                    "Hello world! Book list: book1, book2"
+                                )
+                            )
+                            assertThat(response.content).isEqualTo(expectedBody)
+                        }
+                    }
                 }
             }
         }
     }
-
-    @Nested
-    inner class Post {
-        @Test
-        fun `Post should return success object message with path param`() {
-
-            withTestApplication({
-                baseModule(Level.DEBUG)
-                install(HelloWorldApplication(service, HeadersToProxy()))
-            }) {
-                val body = objectMapper.writeValueAsString(
-                    HelloWorldRequestBody("name", "surname")
-                )
-
-                handleRequest(HttpMethod.Post, "/hello/1234") {
-                    addHeader("Content-Type", "application/json")
-                    setBody(body)
-                }.apply {
-                    assertThat(response.status()?.value).isEqualTo(HttpStatusCode.OK.value)
-
-                    val expectedBody = objectMapper.writeValueAsString(
-                        HelloWorldResponse(
-                            "1234",
-                            null,
-                            "Hello world name surname!"
-                        )
-                    )
-                    assertThat(response.content).isEqualTo(expectedBody)
-                }
-            }
-        }
-
-        @Test
-        fun `Post should return bad request if body is malformed`() {
-            withTestApplication({
-                baseModule(Level.DEBUG)
-                install(HelloWorldApplication(service, HeadersToProxy()))
-            }) {
-                val body = objectMapper.writeValueAsString(
-                    mapOf("name" to "name")
-                )
-
-                handleRequest(HttpMethod.Post, "/hello/1234") {
-                    addHeader("Content-Type", "application/json")
-                    setBody(body)
-                }.apply {
-                    assertThat(response.status()?.value).isEqualTo(HttpStatusCode.BadRequest.value)
-                    val servicePackage = "eu.miaplatform.service"
-                    val expectedBody = objectMapper.writeValueAsString(
-                        // if you change the package name, remember to update it in the following error or the test will fail
-                        ErrorResponse(1000, "Instantiation of [simple type, class $servicePackage.model.request.HelloWorldRequestBody] value failed for JSON property surname due to missing (therefore NULL) value for creator parameter surname which is a non-nullable type\n at [Source: (InputStreamReader); line: 1, column: 15] (through reference chain: $servicePackage.model.request.HelloWorldRequestBody[\"surname\"])")
-                    )
-                    assertThat(response.content).isEqualTo(expectedBody)
-                }
-            }
-        }
-    }
-
-    @Nested
-    @TestInstance(TestInstance.Lifecycle.PER_CLASS)
-    inner class GetWithCall {
-
-        @BeforeEach
-        fun setUp() {
-            mockServer = ClientAndServer.startClientAndServer(port)
-        }
-
-        @AfterEach
-        fun tearDown() {
-            mockServer.close()
-        }
-
-        @Test
-        fun `Get with call should return success object message`() {
-            mockServer.setupResponse(
-                "GET",
-                "/v2/books",
-                200,
-                objectMapper.writeValueAsString(listOf("book1", "book2"))
-            )
-
-            withTestApplication({
-                baseModule(Level.DEBUG)
-                install(HelloWorldApplication(service, HeadersToProxy()))
-            }) {
-                handleRequest(HttpMethod.Get, "/hello/with-call") {
-                }.apply {
-                    assertThat(response.status()?.value).isEqualTo(HttpStatusCode.OK.value)
-
-                    val expectedBody = objectMapper.writeValueAsString(
-                        HelloWorldResponse(
-                            null,
-                            null,
-                            "Hello world! Book list: book1, book2"
-                        )
-                    )
-                    assertThat(response.content).isEqualTo(expectedBody)
-                }
-            }
-        }
-
-        @Test
-        fun `Get with call should return error if call fails`() {
-            mockServer.setupResponse(
-                "GET",
-                "/v2/books",
-                500,
-                "{\"error\": \"error\"}"
-            )
-
-            withTestApplication({
-                baseModule(Level.DEBUG)
-                install(HelloWorldApplication(service, HeadersToProxy()))
-            }) {
-                handleRequest(HttpMethod.Get, "/hello/with-call") {
-                }.apply {
-                    assertThat(response.status()?.value).isEqualTo(HttpStatusCode.InternalServerError.value)
-
-                    val expectedBody = objectMapper.writeValueAsString(
-                        ErrorResponse(1000, "books call failed")
-                    )
-                    assertThat(response.content).isEqualTo(expectedBody)
-                }
-            }
-        }
-
-        @Test
-        fun `Get with call should return success object message with query parameter`() {
-            mockServer.setupResponse(
-                "GET",
-                "/v2/books",
-                200,
-                objectMapper.writeValueAsString(listOf("book1", "book2"))
-            )
-            withTestApplication({
-                baseModule(Level.DEBUG)
-                install(HelloWorldApplication(service, HeadersToProxy()))
-            }) {
-                handleRequest(HttpMethod.Get, "/hello/with-call?queryParam=param") {
-                }.apply {
-                    assertThat(response.status()?.value).isEqualTo(HttpStatusCode.OK.value)
-
-                    val expectedBody = objectMapper.writeValueAsString(
-                        HelloWorldResponse(
-                            null,
-                            "param",
-                            "Hello world! Book list: book1, book2"
-                        )
-                    )
-                    assertThat(response.content).isEqualTo(expectedBody)
-                }
-            }
-        }
-    }
-
-}
+})
